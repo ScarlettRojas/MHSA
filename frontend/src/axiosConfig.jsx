f8243c7af1a4b06df317b7b19d2355992fef11e2
ScarlettRojas <n12084263@qut.edu.au>
2025-08-20 00:41:16 +1000

    frontend: axios baseURL to EC2 public IP:5001

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
-  //baseURL: 'http://3.26.96.188:5001', // live
+  //baseURL: 'http://localhost:5001', // local
+  baseURL: 'http://3.105.229.53:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
